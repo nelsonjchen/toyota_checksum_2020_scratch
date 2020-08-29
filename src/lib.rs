4f8d59c856d02caa332708a983420351a63e13a8
Nelson Chen <crazysim@gmail.com>
2020-08-29 16:27:09 -0700

    ioactive

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
+
+fn toyota_checksum_existing(address: u16, data: u64, length: u8 ) -> u8 {
+    0
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn ioactive() {
+        assert_eq!((0x02 + 0xe4 + 0x05 + 0xf8) & 0xff, 0xe3);
     }
 }
