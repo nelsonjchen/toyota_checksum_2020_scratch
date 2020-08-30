5f3fd4a9522251e2af4a239de1a2708d2c1faaae
Nelson Chen <crazysim@gmail.com>
2020-08-29 17:31:30 -0700

    comma input

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
+fn comma_input(address: u16, data: &[u8]) -> u8 {
+    let address_bytes = address.to_be_bytes();
+    let idh = address_bytes[0];
+    let idl = address_bytes[1];
+    let len = data.len() as u8;
+    let data_trimmed = &data[..((len - 1) as usize)];
+    ioactive_checksum(idh, idl, len, data_trimmed)
+}
+
 fn toyota_checksum_existing(address: u16, data: u64, length: u8) -> u8 {
     0
 }
@@ -16,15 +25,17 @@ mod tests {
     use super::*;
 
     #[test]
-    fn ioactive_from_pdf() {
+    fn test_ioactive_from_pdf() {
         assert_eq!((0x02 + 0xe4 + 0x05 + 0xf8) & 0xff, 0xe3);
     }
 
     #[test]
-    fn ioactive_rust_impl() {
-        assert_eq!(
-            ioactive_checksum(0x02, 0xe4, 0x05, &[0xf8, 0, 0, 0]),
-            0xe3
-        );
+    fn test_ioactive_rust_impl() {
+        assert_eq!(ioactive_checksum(0x02, 0xe4, 0x05, &[0xf8, 0, 0, 0]), 0xe3);
+    }
+
+    #[test]
+    fn test_comma_input() {
+        assert_eq!(comma_input(0x02e4, &[0xf8, 0, 0, 0, 0]), 0xe3);
     }
 }
